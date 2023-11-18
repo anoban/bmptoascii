b956329267305a35aeb0ea46747d928dc1d36700
anoban <2016sb117@univ.jfn.ac.lk>
2023-11-18 13:52:45 +0530

    new file:   ascii.exe 	modified:   include/ascii.h 	new file:   jennifer.txt 	new file:   media/jennifer.bmp 	deleted:    media/moon.bmp 	modified:   media/wineglass.bmp 	modified:   src/main.c

--- include/ascii.h
+++ include/ascii.h
@@ -2,15 +2,15 @@
 #include <bmp.h>
 
 // ASCII characters in descending order of luminance
-static const wchar_t wasciirev[] = { L'N', L'@', L'#', L'W', L'$', L'9', L'8', L'7', L'6', L'5', L'4', L'3', L'2', L'1',
-                                     L'?', L'!', L'a', L'b', L'c', L';', L':', L'+', L'=', L'-', L',', L'.', L'_' };
+static const wchar_t wascii[]     = { L'_', L'.', L',', L'-', L'=', L'+', L':', L';', L'c', L'b', L'a', L'!', L'?', L'1',
+                                      L'2', L'3', L'4', L'5', L'6', L'7', L'8', L'9', L'$', L'W', L'#', L'@', L'N' };
 
-static const wchar_t wascii[]    = { L'_', L'.', L',', L'-', L'=', L'+', L':', L';', L'c', L'b', L'a', L'!', L'?', L'1',
-                                     L'2', L'3', L'4', L'5', L'6', L'7', L'8', L'9', L'$', L'W', L'#', L'@', L'N' };
+static const wchar_t min_wascii[] = { L'@', L'J', L'D', L'%', L'*', L'P', L'+', L'Y', L'$', L',', L'.' };
 
 // Does a weighted averaging on RGB values: (pix.BLUE * 0.299L) + (pix.GREEN * 0.587) + (pix.RED * 0.114)
 static __forceinline wchar_t __stdcall ScaleRgbQuad(_In_ const RGBQUAD* const restrict pixel) {
-    return wascii[((unsigned short) ((pixel->rgbBlue * 0.299L) + pixel->rgbGreen * 0.587L + pixel->rgbRed * 0.114L)) % 27];
+    return min_wascii
+        [((unsigned short) ((pixel->rgbBlue * 0.299L) + pixel->rgbGreen * 0.587L + pixel->rgbRed * 0.114L)) % __crt_countof(min_wascii)];
 }
 
 typedef struct buffer {
@@ -35,24 +35,24 @@ static inline buffer_t GenerateASCIIBuffer(_In_ const WinBMP* const restrict ima
     /*
     In most contemporary .BMP images, the pixel ordering seems to be bottom up.
     i.e,
-                                                                                (pixel at the top right corner of the image)
-                                              10 11 12 13 14 15 16 17 18 19 <-- this will be the last pixel
-    this is the first pixel in the buffer --> 00 01 02 03 04 05 06 07 08 09
-    (pixel at the bottom left corner of the image)
+    (pixel at the top right corner of the image)
+    this will be the last pixel --> 10 11 12 13 14 15 16 17 18 19
+                                    00 01 02 03 04 05 06 07 08 09 <-- this is the first pixel in the buffer
+                                                                     (pixel at the bottom left corner of the image)
     */
 
     // if pixels are ordered top down. i.e the first pixel in the buffer is the one at the top left corner of the image.
-    if (image->infhead.biHeight < 0) { }
+    if (image->infhead.biHeight < 0) { /* TODO */
+    }
 
     // if pixels are ordered bottom up, start the traversal from the last pixel and move up.
     size_t caret = 0;
     if (image->infhead.biHeight >= 0) {
-
-        int64_t ncols = image->infhead.biWidth;
-        for (int64_t nrows = image->infhead.biHeight; nrows > 0; --nrows) {
-            ncols = image->infhead.biWidth;
-            for (; ncols > 0; --ncols) txtbuff[caret++] = ScaleRgbQuad(&image->pixel_buffer[ncols * nrows]);
-            txtbuff[caret] = L'\n';
+        int64_t ncols = 0;
+        for (int64_t nrows = image->infhead.biHeight - 1; nrows >= 0; --nrows) {
+            ncols = image->infhead.biWidth - 1;
+            for (; ncols >= 0; --ncols) txtbuff[caret++] = ScaleRgbQuad(&image->pixel_buffer[ncols * nrows]);
+            txtbuff[caret]   = L'\n';
             txtbuff[++caret] = L'\r';
             caret++;
         }
--- src/main.c
+++ src/main.c
@@ -1,14 +1,14 @@
 #include <ascii.h>
 
 int wmain(_In_opt_ const int32_t argc, _In_opt_count_(argc) wchar_t* argv[]) {
+    uint64_t fsize = 0;
+
 #ifndef _DEBUG
 
     if (argc < 2) {
         fwprintf_s(stderr, L"Not enough arguments: main.exe expects one or more paths to .BMP files\n");
         return EXIT_FAILURE;
     }
-
-    uint64_t fsize = 0;
 
     for (size_t i = 1; i < argc; ++i) {
         const uint8_t* buffer = OpenImage(argv[i], &fsize);
@@ -17,17 +17,15 @@ int wmain(_In_opt_ const int32_t argc, _In_opt_count_(argc) wchar_t* argv[]) {
             const WinBMP image = NewBmpImage(buffer, fsize);
 
             if (image.pixel_buffer) {
-                BmpInfo(&image);
+                // BmpInfo(&image);
 
-                const ascii_t txt = GenerateASCIIBuffer(&image);
+                const buffer_t txt = GenerateASCIIBuffer(&image);
                 if (txt.buffer) {
-                    for (size_t i = 0; i < txt.length; ++i) {
-                        // putwchar(txt.buffer[i]);
-                        // printf_s("%d: %c ", txt.buffer[i], txt.buffer[i]);
-                        putwchar(txt.buffer[i]);
-                    }
+                    _putws(argv[i]);
+                    _putws(txt.buffer);
                     free(txt.buffer);
                 }
+
                 free(image.pixel_buffer);
             }
 
@@ -39,10 +37,6 @@ int wmain(_In_opt_ const int32_t argc, _In_opt_count_(argc) wchar_t* argv[]) {
 
 #else
 
-#endif // !_DEBUG
-
-    uint64_t       fsize  = 0;
-
     const uint8_t* buffer = OpenImage(L"./media/flower.bmp", &fsize);
     const WinBMP   image  = NewBmpImage(buffer, fsize);
     BmpInfo(&image);
@@ -53,5 +47,6 @@ int wmain(_In_opt_ const int32_t argc, _In_opt_count_(argc) wchar_t* argv[]) {
     free(txt.buffer);
     free(image.pixel_buffer);
 
+#endif // !_DEBUG
     return EXIT_SUCCESS;
 }
